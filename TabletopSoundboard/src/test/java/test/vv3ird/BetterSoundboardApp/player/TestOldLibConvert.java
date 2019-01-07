601b6ba37eaba099b33dd52020fca7b8e921fe7c
WElRD <WElRDOverCollect@gmail.com>
2019-01-07 16:37:57 +0100

    Removed unnecessary Testclasses

--- TabletopSoundboard/src/test/java/test/vv3ird/BetterSoundboardApp/player/TestOldLibConvert.java
+++
@@ -1,9 +0,0 @@
-package test.vv3ird.BetterSoundboardApp.player;
-
-import vveird.TabletopSoundboard.AudioApp;
-
-public class TestOldLibConvert {
-	public static void main(String[] args) {
-		AudioApp.convertOldLib();
-	}
-}
